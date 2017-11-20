e29d6b1c6faaaca5c53ce320eefea7748385f60d
Richard Leinfellner <richard@modulo17.com>
2017-11-20 18:00:51 +0000

    FirstUpload

---
+++ Assets/TestAnalitics.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+
+public class TestAnalitics : MonoBehaviour {
+
+
+	int	mClickCount=0;
+
+	float	mTime=0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mTime += Time.deltaTime;
+		
+	}
+
+
+	public	void	TestEvent()
+	{
+		Analytics.CustomEvent ("TestClick", new Dictionary<string, object> {
+			{ "Platform", Application.platform.ToString() },
+			{ "ClickCount", ++mClickCount },
+			{ "DeltaTime", mTime }
+		});
+		mTime = 0;
+	}
+}
